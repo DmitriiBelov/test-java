95242d0e2204ba249774cafc2894aa97f88bfede
DmitriiBelov <ilred@yandex.ru>
2016-10-06 22:16:14 +0300

    Добавлен hello

---
+++ prog1/Test1/app/src/main/java/com/example/test1/Main.java
@@ -0,0 +1,11 @@
+package com.example.test1;
+
+/**
+ * Created by Дима on 06.10.2016.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
