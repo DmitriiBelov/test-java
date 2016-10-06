a9d3ba72e9b869a4290958d238a1664635f48e72
DmitriiBelov <ilred@yandex.ru>
2016-10-06 22:18:10 +0300

    Мелкое исправление

--- prog1/Test1/app/src/main/java/com/example/test1/Main.java
+++ prog1/Test1/app/src/main/java/com/example/test1/Main.java
@@ -6,6 +6,6 @@ package com.example.test1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("Hello!");
     }
 }
