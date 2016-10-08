8a23204178a66765ae6f8209af6ad59f6400d2a7
DmitriiBelov <ilred@yandex.ru>
2016-10-08 12:40:26 +0300

    Работа со строками

--- prog1/Test1/app/src/main/java/com/example/test1/Main.java
+++ prog1/Test1/app/src/main/java/com/example/test1/Main.java
@@ -6,8 +6,15 @@ package com.example.test1;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 4;
-        if (a == 4)
+
+        boolean bRes = true;
+        String sA = new String("string");
+        String sB = new String("string");
+        String sA2 = sA;
+
+        bRes = sA == sB;
+
+        if (bRes)
             System.out.println("It's true!");
         else
             System.out.println("It's false!");
