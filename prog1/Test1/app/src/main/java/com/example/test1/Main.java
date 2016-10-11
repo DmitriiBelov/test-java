a1e5641c6c96699adc6a6f885b27533d785a3501
DmitriiBelov <ilred@yandex.ru>
2016-10-11 22:47:00 +0300

    Обработка исключений

--- prog1/Test1/app/src/main/java/com/example/test1/Main.java
+++ prog1/Test1/app/src/main/java/com/example/test1/Main.java
@@ -1,23 +1,43 @@
 package com.example.test1;
 
+import java.util.Scanner;
+
 /**
  * Created by Дима on 06.10.2016.
  */
 
 public class Main {
     public static void main(String[] args) {
-        Shape crcl = new Circle(5.0f);
-        Shape rctn = new Rectangle(5.0, 3.0);
-        Shape larger = getLargerShape(crcl, rctn);
+        int a = 4;
+        try {
+            System.out.println(a/0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(a);
+        }
 
-        System.out.println("The area: " + larger.area());
-    }
+        // усложнённая обработка исключений
+        /*int[] m= {-1,0,1};
+        Scanner sc = new Scanner(System.in);
+        try {
+            int a = sc.nextInt();
+            m[a] = 4/a;
+            System.out.println(m[a]);
+        } catch (ArithmeticException e) {
+            System.out.println("Произошла недопустимая арифметическая операция");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Обращение по недопустимому индексу массива");
+        } catch (Exception e) {
+            System.out.println("Произошло ещё какое-то исключение");
+        }*/
 
-    public static Shape getLargerShape(Shape s1, Shape s2)
-    {
-        if (s1.area() > s2.area())
-            return s1;
-        else
-            return s2;
+        // простая обработка исключений
+        /*int a = 4;
+        try {
+            System.out.println(a/0);
+        } catch (Exception e) {
+            System.out.println("На ноль делить нельзя!");
+        }*/
     }
 }
