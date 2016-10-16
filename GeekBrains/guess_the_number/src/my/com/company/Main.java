3840531e5c425443ee6ef62f1fccf6289e7b6b05
DmitriiBelov <ilred@yandex.ru>
2016-10-16 23:06:56 +0300

    Медоты и отладка.

--- GeekBrains/guess_the_number/src/my/com/company/Main.java
+++ GeekBrains/guess_the_number/src/my/com/company/Main.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
+    private  static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int iRange = 10;
         int iNumber = (int)(Math.random() * iRange);
+        playLevel(iRange, iNumber);
+        scanner.close();
+    }
+
+    private static void playLevel(int iRange, int iNumber) {
         while (true) {
-            System.out.println("Угадай число от 0 до " + iRange);
+            System.out.println("Угадай число от 0 до " + iRange + "(" + iNumber + ")");
             int iNumberUser = scanner.nextInt();
             if (iNumber == iNumberUser) {
                 System.out.println("Угадал число!");
                 break;
+            } else if (iNumber < iNumberUser) {
+                System.out.println("Загаданное число меньше!");
             } else {
-                System.out.println("Не угадал число!");
+                System.out.println("Загаданное число больше!");
             }
         }
-        scanner.close();
     }
 }
