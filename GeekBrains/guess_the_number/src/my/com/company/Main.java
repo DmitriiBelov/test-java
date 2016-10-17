eaeb9c636eb4d2d18cbb0f04a7d30ca10313aca5
DmitriiBelov <ilred@yandex.ru>
2016-10-17 22:07:44 +0300

    Циклы.

--- GeekBrains/guess_the_number/src/my/com/company/Main.java
+++ GeekBrains/guess_the_number/src/my/com/company/Main.java
@@ -7,15 +7,15 @@ public class Main {
     private  static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int iRange = 10;
-        int iNumber = (int)(Math.random() * iRange);
-        playLevel(iRange, iNumber);
+        for(int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!");
         scanner.close();
     }
 
-    private static void playLevel(int iRange, int iNumber) {
+    private static void playLevel(int iRange) {
+        int iNumber = (int) (Math.random() * iRange);
         while (true) {
-            System.out.println("Угадай число от 0 до " + iRange + "(" + iNumber + ")");
+            System.out.println("Угадай число от 0 до " + iRange + " (" + iNumber + ")");
             int iNumberUser = scanner.nextInt();
             if (iNumber == iNumberUser) {
                 System.out.println("Угадал число!");
