0069f448f3d731fc2cdb9b537f2f8073abf5cf2c
DmitriiBelov <ilred@yandex.ru>
2016-11-01 22:32:12 +0300

    Шаблонные функции.

--- VS60_exp_cpp/test_cons/test_cons.cpp
+++ VS60_exp_cpp/test_cons/test_cons.cpp
@@ -6,14 +6,26 @@
 
 using namespace std;
 
+template <typename T1, typename T2>
+T2 printArr(const T1 * array, int i) {
+	int count = 0;
+	for (int j=0; j<i; j++) {
+		cout << array[j] << " ";
+		count++;
+	}
+	cout << endl;
+	return count;
+}
 
 int main(int argc, char* argv[])
 {
-	int value = 8;
-	int &ref = value;
+	const int iSize = 3, cSize = 5;
 
-	cin >> ref;
-	cout << value << " - " << ref << endl;
+	int i_arr[iSize] = {10, 33, 55};
+	char c_arr[cSize] = {"Hii!"};
+
+	cout << "count: " << printArr<int, int>(i_arr, iSize) << endl;
+	cout << "count: " << printArr<char, int>(c_arr, cSize) << endl;
 
 	return 0;
 }
