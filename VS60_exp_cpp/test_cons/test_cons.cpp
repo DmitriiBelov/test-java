6c409552815d2703ec5302b5a20c5e9a6bc7d208
DmitriiBelov <ilred@yandex.ru>
2016-11-01 22:54:52 +0300

    Динамический массив

--- VS60_exp_cpp/test_cons/test_cons.cpp
+++ VS60_exp_cpp/test_cons/test_cons.cpp
@@ -8,30 +8,15 @@ using namespace std;
 
 const int arr_length =3;
 
-template <class Type>
-
-class Arr {
-private:
-	Type arr[arr_length];
-public:
-	Arr(Type *a) {
-		for(int i=0; i<arr_length; i++)
-			arr[i]=a[i];
-	}
-	Type getElement(int i) {
-		return arr[i];
-	}
-};
 
 int main(int argc, char* argv[])
 {
-	int arr1[]={12,23,45,6};
-	Arr<int> obj(arr1);
-	cout << obj.getElement(2) << endl;
-
-	char arr2[]={"Mars!"};
-	Arr<char> obj2(arr2);
-	cout << obj2.getElement(0) << endl;
+	int *iArr = new int[6];
+	for(int i=0;i<6;i++)
+		iArr[i]=i*i;
+	for(i=0;i<6;i++)
+		cout << iArr[i] << " ";
+	delete [] iArr;
 
 	return 0;
 }
