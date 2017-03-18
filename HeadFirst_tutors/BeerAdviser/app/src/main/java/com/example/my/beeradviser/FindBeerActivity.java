42ef377476adae99b707935e4ab3729a5fffdd1b
DmitriiBelov <ilred@yandex.ru>
2017-03-18 20:23:43 +0300

    Добавлен список, обработка кнопки - выбор из списка и вывод этого выбора.

--- HeadFirst_tutors/BeerAdviser/app/src/main/java/com/example/my/beeradviser/FindBeerActivity.java
+++ HeadFirst_tutors/BeerAdviser/app/src/main/java/com/example/my/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.example.my.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        brands.setText(String.valueOf(color.getSelectedItem()));
     }
 }
